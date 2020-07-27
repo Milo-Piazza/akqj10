3e10e5dc184cb397a0085257359c25c233d53024
Milo Piazza <mpiazzagithub@gmail.com>
2020-07-26 18:44:38 -0700

    Add Deck test

---
+++ src/model/Deck.test.js
@@ -0,0 +1,12 @@
+const Deck = require("./Deck");
+
+test ("Test drawing all 52 cards with no duplicates", () => {
+    const deck = new Deck();
+    const cards = new Set();
+    for (let i = 0; i < 52; i++) {
+        const card = deck.drawCard();
+        const cardstr = card.rank + card.suit;
+        expect(cards.has(cardstr)).toBeFalsy();
+        cards.add(cardstr);
+    }
+})
